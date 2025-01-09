156cc866596d50cae29cc92b4d8d4df6e5797991
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 10:39:44 +0530

    square_root

---
+++ gitpush.ps1
@@ -0,0 +1,12 @@
+# Prompt the user for the commit message
+$commitMessage = Read-Host "Enter your commit message"
+ 
+# Stage all changes
+git add .
+ 
+# Commit with the provided message
+git commit -m $commitMessage
+ 
+# Push the changes
+git push
+ 
