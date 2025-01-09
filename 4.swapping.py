4d86bd4b53edfe401351340e0aec46a8d8e6ebb9
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 11:12:50 +0530

    swapping

---
+++ 4.swapping.py
@@ -0,0 +1,16 @@
+# without using third variable
+a=10
+b=20
+a=a+b         
+b=a-b         
+a=a-b          
+print("a=",a ,"b=",b)
+
+
+# using third variable
+a1=10
+b1=20
+c=a1+b1
+a1=c-a1
+b1=c-b1
+print("a1=",a1 ,"b1=",b1)
