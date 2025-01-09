389fa42c2dc5eb184519ae30e4f619a652684eca
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 11:20:49 +0530

    swapping

--- 4.swapping.py
+++ 4.swapping.py
@@ -10,7 +10,7 @@ print("a=",a ,"b=",b)
 # using third variable
 a1=10
 b1=20
-c=a1+b1
-a1=c-a1
-b1=c-b1
+temp=a1       #10
+a1=b1         #a1=10
+b1=temp
 print("a1=",a1 ,"b1=",b1)
---
+++ 5.km_to_miles.py
@@ -0,0 +1,3 @@
+km=float(input("Enter how many kilometer you want to convert into miles: "))
+convert=km*0.621371
+print(f"{km} is converted into",convert,"miles")
