669f6807399301eb5e523313d0bf221ed861d17f
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 10:37:04 +0530

    first commit

---
+++ add two numbers.py
@@ -0,0 +1,13 @@
+# 1. pre-defined variables
+num1=4
+num2=3
+sum=num1+num2
+print(f"sum of {num1} & {num2} is : ",sum)
+
+
+
+# 2. taking input from user
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+c=a+b
+print(f"sum of {a} & {b} is : ",c)
