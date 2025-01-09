6fac80c65f07456d929dfe68d22c38c6dc33aff7
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 12:31:34 +0530

    fibbonacci

---
+++ 16.fibbonacci.py
@@ -0,0 +1,15 @@
+n=int(input("Enter any number: "))
+n1,n2=0,1
+count=0
+if n<=0:
+    print("Enter positive number")
+elif n==1:
+    print("fibonacci series upto:",n)
+    print(n1)    
+else:
+    while count<n:
+        print(n1)
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        count+=1    
