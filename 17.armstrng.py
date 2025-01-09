f6f6c82d10849d289f9fbb8d726c0e64b8793c2f
Bhumi016 <vbhumika395@gmail.com>
2025-01-09 16:07:06 +0530

    armstrong

---
+++ 17.armstrng.py
@@ -0,0 +1,13 @@
+n=int(input("enter number: "))
+sum=0
+temp=n
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+
+if n==sum:
+    print(f"the {n} is armstrong number")
+else:
+    print(f"the {n} is not armstrong number")        
+
